38625bcd868883049c31f0b55dccd5d80d1d4eda
andrkavr <72137459+andrkavr@users.noreply.github.com>
2021-06-24 16:19:26 +0200

    completed Part02 Exercise18

--- src/Part02_15_CountingToHundred/CountingToHundred.java
+++ src/Part02_15_CountingToHundred/CountingToHundred.java
@@ -1,4 +1,17 @@
 package Part02_15_CountingToHundred;
 
+import java.util.Scanner;
+
 public class CountingToHundred {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = scanner.nextInt();
+
+        for (int i = number; i <= 100; i++) {
+            System.out.println(i);
+        }
+
+    }
 }
