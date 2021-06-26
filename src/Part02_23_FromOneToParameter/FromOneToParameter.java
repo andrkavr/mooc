3b0e2e9cb4917a905b2db810fa410d146e607f85
andrkavr <72137459+andrkavr@users.noreply.github.com>
2021-06-26 15:10:47 +0200

    Completed Part02_Exercise23

---
+++ src/Part02_23_FromOneToParameter/FromOneToParameter.java
@@ -0,0 +1,15 @@
+package Part02_23_FromOneToParameter;
+
+public class FromOneToParameter {
+
+    public static void main(String[] args) {
+        printUntilNumber(5);
+    }
+
+    public static void printUntilNumber(int number) {
+
+        for (int i = 1; i <= number; i++) {
+            System.out.println(i);
+        }
+    }
+}
