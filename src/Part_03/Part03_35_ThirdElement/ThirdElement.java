9a9cc4e3229964590bc9ec49257c5a9a9d314199
andrkavr <72137459+andrkavr@users.noreply.github.com>
2021-06-29 11:55:55 +0200

    completed Part 03 Exercise 36

---
+++ src/Part_03/Part03_35_ThirdElement/ThirdElement.java
@@ -0,0 +1,23 @@
+package Part_03.Part03_35_ThirdElement;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ThirdElement {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<String> list = new ArrayList<>();
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+
+            list.add(input);
+        }
+
+        System.out.println(list.get(2));
+    }
+}
