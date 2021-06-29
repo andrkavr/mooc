263c6b7986eb5ae2c88a3e0868c3d66e85c82254
andrkavr <72137459+andrkavr@users.noreply.github.com>
2021-06-29 14:36:20 +0200

    completed Part 03 Exercise 05

---
+++ src/Part_03/Part_03_05_LastInList/LastInList.java
@@ -0,0 +1,25 @@
+package Part_03.Part_03_05_LastInList;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class LastInList {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<String> list = new ArrayList<>();
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+
+            list.add(input);
+        }
+
+        for (int i = list.size() - 1; i >= list.size() - 1; i--) {
+            System.out.println(list.get(i));
+        }
+    }
+}
